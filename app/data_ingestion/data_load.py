2cb99a04f2b2c662780548dc144ac0af60f427eb
saeed-cefalo <saeed.anwar@cefalo.com>
2023-05-23 14:31:41 +0600

    DIF-22: Pass the configuration to the loader instead of parameters
    - Change the supabase loader to general postgres loader

--- app/data_ingestion/data_load.py
+++ app/data_ingestion/data_load.py
@@ -4,9 +4,9 @@ import sqlalchemy
 DATA_DEFAULT_LOCATION = './'
 
 
-class LoadDataToSupabase:
+class LoadDataToPostgres:
 
-    def load_data(self, file_name, table: str, db_reference_id: str, db_password: str):
+    def load_data(self, config):
         """
             Utility function to insert a Dataframe to a table in Supabase postgres DB.
             If the table does not exist, it will be created.
@@ -18,10 +18,21 @@ class LoadDataToSupabase:
                 db_password (str): The password of the supabase db
         """
         try:
+            file_name = config['file_name']
+            table = config['table_name']
+            dest = config['destination']
+            if_exists = config['if_exists']
+
+            db = dest['database']
+            user = dest['username']
+            password = dest['password']
+            host = dest['host']
+            port = dest['port']
+
             engine = sqlalchemy.create_engine(
-                f'postgresql://postgres:{db_password}@db.{db_reference_id}.supabase.co:5432/postgres')
+                f'postgresql://{user}:{password}@{host}:{port}/{db}')
             dataframe = pd.read_csv(DATA_DEFAULT_LOCATION + file_name, index_col=False)
-            dataframe.to_sql(table, con=engine, if_exists="append", index=False)
+            dataframe.to_sql(table, con=engine, if_exists=if_exists, index=False)
             print(f"Data loaded into table: {table}")
         except Exception as E:
             error_msg = '{}: Data insertion failed {}'.format(table, E)
@@ -32,7 +43,7 @@ class LoadDataToSupabase:
 def loader(destination="Supabase"):
     """Factory Method"""
     loaders = {
-        "Supabase": LoadDataToSupabase,
+        "postgres": LoadDataToPostgres,
         # "Google-Cloud": ExtractGoogleCloudData
     }
 
